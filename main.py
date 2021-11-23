c5a8ee6f7948461811a4c15ac4f12ad54e96b4c4
jun_neng <soojunneng01@gmail.com>
2021-11-23 16:37:08 +0800

    -m i

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import datetime
+import readfile as rf
+
 import constants as keys
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler
+import telegram
 from telegram import Update, Bot
 import responses as R
+
+
+# https://stackoverflow.com/questions/62445753/how-to-send-pdf-file-back-to-user-using-python-telegram-bot - sending file back to user
+# https://stackoverflow.com/questions/31096358/how-do-i-download-a-file-or-photo-that-was-sent-to-my-telegram-bot - downloading file sent to bot
 
 
 print("Bot Started...")
@@ -15,8 +23,19 @@ def help_command(update: Update, context: CallbackContext):
     update.message.reply_text("Lame...")
 
 
+def workout_command(update: Update, context: CallbackContext):
+    text = f"```{rf.get_workout_by_date(str(datetime.date.today()) + ' 00:00:00')}```"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=telegram.ParseMode.MARKDOWN)
+    # context.bot.send_message(chat_id=update.effective_chat.id, text='test')
+
+
 def handle_message(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+def downloader(update: Update, context: CallbackContext):
+    with open(f"{update.message.from_user['id']}.xlsx", 'wb') as f:
+        context.bot.get_file(update.message.document).download(out=f)
 
 
 def error(update, context):
@@ -28,7 +47,9 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start_command))
     dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("workout", workout_command))
     dp.add_handler(MessageHandler(Filters.text, handle_message))
+    dp.add_handler(MessageHandler(Filters.document, downloader))
     updater.start_polling()
     updater.idle()
 
