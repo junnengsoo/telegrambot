54fde32e0aaff9e181737cfc2375b50d32e501bf
jun_neng <soojunneng01@gmail.com>
2021-11-23 17:35:23 +0800

    alternate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import readfile as rf
+import requests as rs
 
 import constants as keys
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler
@@ -10,7 +11,7 @@ import responses as R
 
 # https://stackoverflow.com/questions/62445753/how-to-send-pdf-file-back-to-user-using-python-telegram-bot - sending file back to user
 # https://stackoverflow.com/questions/31096358/how-do-i-download-a-file-or-photo-that-was-sent-to-my-telegram-bot - downloading file sent to bot
-
+global res
 
 print("Bot Started...")
 
@@ -26,16 +27,18 @@ def help_command(update: Update, context: CallbackContext):
 def workout_command(update: Update, context: CallbackContext):
     text = f"```{rf.get_workout_by_date(str(datetime.date.today()) + ' 00:00:00')}```"
     context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=telegram.ParseMode.MARKDOWN)
-    # context.bot.send_message(chat_id=update.effective_chat.id, text='test')
 
 
 def handle_message(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-
-def downloader(update: Update, context: CallbackContext):
-    with open(f"{update.message.from_user['id']}.xlsx", 'wb') as f:
-        context.bot.get_file(update.message.document).download(out=f)
+    csv_url = update.message.text
+    csv_file = rs.get(url=csv_url)
+    rf.create_table(csv_file)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I have received your Google Sheets!")
+#
+#
+# def downloader(update: Update, context: CallbackContext):
+#     with open(f"{update.message.from_user['id']}.xlsx", 'wb') as f:
+#         context.bot.get_file(update.message.document).download(out=f)
 
 
 def error(update, context):
@@ -49,7 +52,7 @@ def main():
     dp.add_handler(CommandHandler("help", help_command))
     dp.add_handler(CommandHandler("workout", workout_command))
     dp.add_handler(MessageHandler(Filters.text, handle_message))
-    dp.add_handler(MessageHandler(Filters.document, downloader))
+    # dp.add_handler(MessageHandler(Filters.document, downloader))
     updater.start_polling()
     updater.idle()
 
--- readfile.py
+++ readfile.py
@@ -7,31 +7,33 @@ import os
 # storing of data into database using sqlite
 # not sure if check_same_thread=False will cause any problems
 # https://www.youtube.com/watch?v=pd-0G0MigUA&t=1566s&ab_channel=CoreySchafer
-conn = sqlite3.connect(':memory:', check_same_thread=False)
+conn = sqlite3.connect('test1.db', check_same_thread=False)
 c = conn.cursor()
 
 
 # reading excel sheet that is passed into the program using pandas
 # https://www.youtube.com/watch?v=vmEHCJofslg&t=453s&ab_channel=KeithGalli
-if Update.message.document is True:
-    user_id = Update.message.from_user['id']
-    df_xlsx = pd.read_excel(f'{user_id}', header=0)
-    print(df_xlsx)
-    # Updates sql database from xlsx
-    with conn:
-        c.execute(f"""CREATE TABLE IF NOT EXISTS {user_id} (
-                    Exercise,
-                    Sets,
-                    Reps,
-                    Intensity,
-                    Load,
-                    Tempo,
-                    Rest
-                    )""")
-        df_xlsx.to_sql(f'{user_id}', conn, if_exists='append', index=False)
-
-else:
-    pass
+def create_table(csv_file):
+    if Update.message is True:
+        try:
+            user_id = Update.message.from_user['id']
+            print(csv_file)
+            # Updates sql database from xlsx
+            with conn:
+                c.execute(f"""CREATE TABLE IF NOT EXISTS {user_id} (
+                            Exercise,
+                            Sets,
+                            Reps,
+                            Intensity,
+                            Load,
+                            Tempo,
+                            Rest
+                            )""")
+                csv_file.to_sql(f'{user_id}', conn, if_exists='append', index=False)
+        finally:
+            pass
+    else:
+        pass
 
 
 # dataframe needs to be converted to string in order to be printed as a message
