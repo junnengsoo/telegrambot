57b0ecbbbaf586625ca1ea8c23d3e2cede3b76ae
jun_neng <soojunneng01@gmail.com>
2021-11-30 16:20:09 +0800

    deployment

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import datetime
 import os
-import constants as keys
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler
 import telegram
 from telegram import Update
@@ -17,7 +16,7 @@ import urllib.request
 
 
 PORT = int(os.environ.get('PORT', 5000))
-
+API_KEY = "2131896364:AAH6u2F3__TmQ7gIKNZpJA-K1HPajSQmFAA"
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -82,7 +81,7 @@ def error(update: Update, context: CallbackContext):
 
 
 def main():
-    updater = Updater(token=keys.API_KEY, use_context=True)
+    updater = Updater(token=API_KEY, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start_command))
     dp.add_handler(CommandHandler("help", help_command))
@@ -91,8 +90,8 @@ def main():
     dp.add_error_handler(error)
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=keys.API_KEY)
-    updater.bot.setWebhook('https://calm-wildwood-33996.herokuapp.com/' + keys.API_KEY)
+                          url_path=API_KEY)
+    updater.bot.setWebhook('https://calm-wildwood-33996.herokuapp.com/' + API_KEY)
     updater.idle()
 
 
@@ -106,7 +105,7 @@ def main():
 # https://www.youtube.com/watch?v=pd-0G0MigUA&t=1566s&ab_channel=CoreySchafer
 
 
-conn = sqlite3.connect('test1.db', check_same_thread=False)
+conn = sqlite3.connect('main.db', check_same_thread=False)
 c = conn.cursor()
 
 
